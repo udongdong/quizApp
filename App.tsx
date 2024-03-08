9107ca31da67bcc8dca0544ba4a1a0d1dfb0aaab
youdong-themoin <70510016+youdong-themoin@users.noreply.github.com>
2024-03-08 15:15:47 +0900

    unused library removed. (recoil)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import MainScreen from './src/screens/MainScreen';
-import {RecoilRoot} from 'recoil';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
 export default function App(): React.JSX.Element {
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <SafeAreaView style={styles.main}>
-          <MainScreen />
-        </SafeAreaView>
-      </QueryClientProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView style={styles.main}>
+        <MainScreen />
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
 
